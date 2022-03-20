83b14f28ef09dd5a0efe52ee904ce9fdf6e84fc8
0xFlagPlz <59506742+AchocolatechipPancake@users.noreply.github.com>
2022-03-19 23:27:52 -0700

    Update netsweeper.sh

--- netsweeper.sh
+++ netsweeper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [[ "$1" == "" ]]  
 then
-  echo "[*] - ERROR: Wrong Format\n**Only Scans /24**\n\tUsage: ./netsweeper 192.168.0"
+  echo "[*] - ERROR: Wrong Format\n**Only Scans /24**\n\tUsage: ./netsweeper.sh 192.168.0"
 else
   for i in {1..255} ; do ping -c 1 $1.$i | grep "64 bytes" & done 2> /dev/null | awk '{print $4}' | cut -d ":" -f 1
 fi
